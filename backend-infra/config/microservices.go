5a8404f2d44658af1566a08480b3634b93effe4e
Martunis <martunismukhtar@gmail.com>
2025-09-14 07:53:04 +0700

    fix:integrasi ke backend-infra

--- backend-infra/config/microservices.go
+++ backend-infra/config/microservices.go
@@ -5,17 +5,24 @@ import (
 )
 
 type ServiceURLs struct {
-	AuthServiceURL string
+	AuthServiceURL      string
+	PROFILE_SERVICE_URL string
 	// Add more as needed
 }
 
 func LoadServiceURLs(config *viper.Viper) *ServiceURLs {
 	AuthServiceURL := config.GetString("AUTH_SERVICE_URL")
+	PROFILE_SERVICE_URL := config.GetString("PROFILE_SERVICE_URL")
 	if AuthServiceURL == "" {
 		AuthServiceURL = "http://localhost:3001"
 	}
 
+	if PROFILE_SERVICE_URL == "" {
+		PROFILE_SERVICE_URL = "http://localhost:3002"
+	}
+
 	return &ServiceURLs{
-		AuthServiceURL: AuthServiceURL,
+		AuthServiceURL:      AuthServiceURL,
+		PROFILE_SERVICE_URL: PROFILE_SERVICE_URL,
 	}
 }
